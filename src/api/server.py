f960f2a09c753a1f0e9f26459bd752456b133cae
rb58853 <rb58853@gmail.com>
2025-12-07 19:04:58 -0500

    Add Filter Response prompt

--- src/api/server.py
+++ src/api/server.py
@@ -13,6 +13,7 @@ from ..prompts import get_system_prompt
 base_prompt: str = get_system_prompt("base_prompt")
 md: str = get_system_prompt("md")
 no_sql: str = get_system_prompt("no_sql")
+filter_response: str = get_system_prompt("filter_response")
 # ------------------------------------------------
 
 # ------------------------------------------------
